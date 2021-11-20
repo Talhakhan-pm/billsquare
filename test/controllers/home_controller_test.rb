5aa1602a12d9e329b77829c9ff331d43f382861b
Khan <Talhaakhan@pm.me>
2021-11-19 23:53:24 -0800

    first commit

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+
+  test "should get pricing" do
+    get home_pricing_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get home_contact_url
+    assert_response :success
+  end
+end
